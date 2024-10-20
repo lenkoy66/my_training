8ac5cb1205991be4587ed4088431157052c81154
lenkoy66 <lenkoy66@gmail.com>
2024-10-20 11:23:03 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,10 @@
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+
+    return matrix
+
+print(get_matrix(3,5,42))
